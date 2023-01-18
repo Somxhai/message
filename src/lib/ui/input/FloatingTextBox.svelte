0d25cb90c3539d91a87392532dceb0a1f09fe8cd
= <=>
2023-01-18 23:30:41 +0700

    Add floating button

---
+++ src/lib/ui/input/FloatingTextBox.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { afterUpdate } from "svelte";
+
+  export let name: string;
+  export let type: string;
+  export let hint: string;
+  let label: HTMLLabelElement;
+  let textBox: HTMLInputElement;
+  afterUpdate(() => {
+    onAutofill();
+  });
+  const onAutofill = () => {
+    // checking for autofill
+    if (textBox.matches(":-webkit-autofill")) {
+      onFloating();
+    }
+  };
+  const onFloating = () => {
+    label.style.top = "-0.5rem";
+    label.style.fontSize = "0.75rem";
+    label.style.color = "rgb(29 78 216)";
+  };
+  const onStopFloating = () => {
+    if (textBox.value.length) {
+      return;
+    }
+    label.style.top = "1rem";
+    label.style.fontSize = "1rem";
+    label.style.color = "";
+  };
+</script>
+
+<div class="relative">
+  <label
+    for={name}
+    bind:this={label}
+    class=" bg-white text-gray-500 rounded-sm px-1 ">{hint}</label
+  >
+  <input
+    bind:this={textBox}
+    {type}
+    {name}
+    class="border-1 p-4 rounded-md "
+    on:focus={onFloating}
+    on:focusout={onStopFloating}
+  />
+</div>
+
+<style>
+  label {
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    font-size: 1rem;
+    transition: top 0.3s, font-size 0.3s;
+  }
+</style>
