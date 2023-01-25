5d7cfe3e3f41fbb5b0119f296f0bef65989303c8
= <=>
2023-01-26 01:35:14 +0700

    format

--- src/lib/utils/clickOutside.ts
+++ src/lib/utils/clickOutside.ts
@@ -1,20 +1,17 @@
-export function clickOutside(node:Node) {
-  
-    const handleClick = (event:MouseEvent) => {
-        const element =  event.target as HTMLButtonElement
-      if (node && !node.contains() && !event.defaultPrevented) {
-        node.dispatchEvent(
-          new CustomEvent('click_outside', node)
-        )
-      }
+// I don't know what type should I assign :(
+export function clickOutside(node: Node) {
+  const handleClick = (event: MouseEvent) => {
+    const element = event.target as HTMLButtonElement;
+    if (node && !node.contains(element) && !event.defaultPrevented) {
+      node.dispatchEvent(new CustomEvent("click_outside", node));
     }
-  
-      document.addEventListener('click', handleClick, true);
-    
-    return {
-      destroy() {
-        document.removeEventListener('click', handleClick, true);
-      }
-      }
-  }
-  
+  };
+
+  document.addEventListener("click", handleClick, true);
+
+  return {
+    destroy() {
+      document.removeEventListener("click", handleClick, true);
+    },
+  };
+}
