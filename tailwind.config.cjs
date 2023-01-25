fc64ac2af0384f613436fec978b9d090968fab21
= <=>
2023-01-26 01:28:52 +0700

    add skeleton animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,18 +3,31 @@ module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     borderWidth: {
-      DEFAULT: '1px',
-      '1': '1px',
-      '2': '2px',
-      '3': '3px',
-      '4': '4px',
-      '6': '6px',
-      '8': '8px',
+      DEFAULT: "1px",
+      1: "1px",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
     },
     extend: {
       fontFamily: {
         kanit: ["Kanit", "sans-serif"],
         plus: ['"Plus Jakarta Sans"', "sans-serif"],
+      },
+      animation: {
+        skeleton: "skeleton-loading 1s linear infinite alternate",
+      },
+      keyframes: {
+        "skeleton-loading": {
+          "0%": {
+            backgroundColor: "hsl(200, 20%, 80%)",
+          },
+          "100%": {
+            backgroundColor: "hsl(200, 20%, 95%)",
+          },
+        },
       },
     },
   },
