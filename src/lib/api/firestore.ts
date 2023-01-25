9bc010b511feba6f6ca5dea7669b9dc2ea7a58e0
= <=>
2023-01-26 02:43:42 +0700

    added error handler

--- src/lib/api/firestore.ts
+++ src/lib/api/firestore.ts
@@ -1,4 +1,5 @@
 import type { FireStoreData } from "$lib/types";
+import { error } from "@sveltejs/kit";
 
 export const updateData = async (uid: string, data: object) => {
   const result = await fetch("/database/userstore", {
@@ -11,8 +12,10 @@ export const updateData = async (uid: string, data: object) => {
   return result;
 };
 
-export const getUserData = async (uid:string) => {
-  
+export const getUserData = async (uid: string) => {
+  if (!uid) {
+    throw error(404, { message: "uid is not found" });
+  }
   const result = await fetch("/database/userstore", {
     method: "POST",
     body: JSON.stringify({ uid }),
