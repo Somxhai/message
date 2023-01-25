e5f695f8ac79f82a238249f0dffafc239efa3eed
= <=>
2023-01-26 01:30:21 +0700

    format

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,7 +1,5 @@
 import { browser } from "$app/environment";
 import type { FirebaseApp, FirebaseOptions } from "firebase/app";
-
-
 import { readable } from "svelte/store";
 
 const firebaseConfig: FirebaseOptions = {
@@ -18,23 +16,22 @@ const firebaseConfig: FirebaseOptions = {
 // load the firebase app on demand by putting it in a store
 // this can then be used in derived stores for auth, firestore, and other services
 
-
 function createApp() {
-  let app: FirebaseApp
+  let app: FirebaseApp;
 
   const { subscribe } = readable<FirebaseApp>(undefined, (set) => {
     async function init() {
       if (!app) {
-        const { initializeApp } = await import('firebase/app')
-        app = initializeApp(firebaseConfig)
+        const { initializeApp } = await import("firebase/app");
+        app = initializeApp(firebaseConfig);
       }
-      set(app)
+      set(app);
     }
 
-    if (browser) init()
-  })
+    if (browser) init();
+  });
 
-  return { subscribe }
+  return { subscribe };
 }
 
-export const app = createApp()
+export const app = createApp();
