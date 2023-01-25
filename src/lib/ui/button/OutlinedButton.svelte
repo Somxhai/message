b919c081b99dc9ca84e657296658c25b7780f60c
= <=>
2023-01-26 01:35:21 +0700

    format

--- src/lib/ui/button/OutlinedButton.svelte
+++ src/lib/ui/button/OutlinedButton.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
-    import { Circle } from "svelte-loading-spinners";
-    const dispatch = createEventDispatcher();
-    export let loading: boolean = false;
-    export let className: string = "";
-    export let type: "button" | "submit" | "reset" = "button";
-    const handleClick = () => {
-      dispatch("click");
-    };
-  
-    export { className as class };
-  </script>
+  import { createEventDispatcher } from "svelte";
+  import { Circle } from "svelte-loading-spinners";
+  const dispatch = createEventDispatcher();
+  export let loading: boolean = false;
+  export let className: string = "";
+  export let type: "button" | "submit" | "reset" = "button";
+  const handleClick = () => {
+    dispatch("click");
+  };
 
+  export { className as class };
+</script>
 
 <button
   {type}
@@ -19,10 +18,9 @@
   class={`rounded-md bg-transparent outline-2 outline-black outline  font-kanit text-sm text-black ${className}`}
 >
   {#if loading}
-  <div class="mx-auto w-fit">
-     <Circle size="1.25" unit="rem" color="white" />
-  </div>
-   
+    <div class="mx-auto w-fit">
+      <Circle size="1.25" unit="rem" color="white" />
+    </div>
   {:else}
     <slot />
   {/if}
