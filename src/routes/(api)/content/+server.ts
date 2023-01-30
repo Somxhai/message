ba82ccaefb449e7b3dc2a25d7dc1c7f79d9ce226
= <=>
2023-01-30 07:28:59 +0700

    allow to get all documents in users collection

--- src/routes/(api)/content/+server.ts
+++ src/routes/(api)/content/+server.ts
@@ -1,31 +1,21 @@
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
-import { firestore } from "firebase-admin";
 import type { FireStoreData } from "$lib/types";
-import { getContentById } from "$lib/api/content.server.";
+import { getContentById } from "$lib/api/server/content.server.";
 
 export const POST = (async ({ request, fetch }) => {
   const body = await request.json();
   const { uid, all } = body;
-  const db = firestore();
-  
+
   // get all messages for message page
   if (all) {
-    const collection = await db.collection("users").get();
-    const collections = [];
-    for (const val of collection.docs) {
-      const uid: string = val.id;
-      const userdata = val.data() as FireStoreData;
-      const content = {
-        uid,
-        userdata,
-      };
-      collections.push(content);
-    }
+    const collectionsRequest = await fetch("/database/userstore", {
+      method: "GET",
+    });
+    const collections = JSON.parse(await collectionsRequest.text());
     return json(collections);
   }
 
-  
   const userstore = await fetch("/database/userstore", {
     method: "POST",
     body: JSON.stringify({
