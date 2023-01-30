561d646857d2e046c785733f53913b0e7294863c
= <=>
2023-01-30 07:23:15 +0700

    init

---
+++ src/lib/component/AdminMessage.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let username:string
+    export let message:string;
+    export let className: string = ""
+    export {className as class}
+</script>
+
+
+
+<div class="{className}">
+    <p class="text-blue-600 font-medium text-2xl">{username}</p>
+    <p class="my-1">{message}</p>
+</div>
