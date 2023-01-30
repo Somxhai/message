4300a54ca97acc9ec724774785692ad27cd002a0
= <=>
2023-01-30 07:26:48 +0700

    init

---
+++ src/lib/component/ShowAmount.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let title: string;
+  export let amount: number;
+  export let className: string = "";
+
+  export { className as class };
+</script>
+
+<div class="text-center font-kanit px-4 py-2 hover:bg-gray-100 whitespace-nowrap {className}">
+    <h4 class="text-gray-600 text-xs md:text-md">{title}</h4>
+    <h1 class="text-lg md:text-2xl">{amount}</h1>
+</div>
