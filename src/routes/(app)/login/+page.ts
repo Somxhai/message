ae9b5d24bd0be45c90febe9bf6210a8fa94eb27e
= <=>
2023-01-30 07:30:27 +0700

    init

---
+++ src/routes/(app)/login/+page.ts
@@ -0,0 +1,18 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+
+
+
+
+export const load: PageLoad = async ({fetch}) => {
+    const req = await fetch('/session', {
+        method: "GET"
+    })
+    
+    const cookie = JSON.parse(await req.text())
+    if (cookie.cookie) {
+        throw redirect(301, '/account');
+    }
+    return {cookie}
+};
