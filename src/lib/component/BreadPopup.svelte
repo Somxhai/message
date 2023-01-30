7dd9c1ee0ae33f0400d5961afc3d7c1f0fc5043f
= <=>
2023-01-30 07:23:22 +0700

    init

---
+++ src/lib/component/BreadPopup.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  export let text: string;
+  export let className: string = "";
+
+  export { className as class };
+  export let second: number = 2;
+  let toggle = false;
+  export const onToggle = () => {
+    toggle = true;
+    setTimeout(() => toggle = false, second * 1000);
+  };
+</script>
+
+{#if toggle}
+  <div
+    transition:fade
+    class="fixed  top-9 z-50 left-1/2 px-4 py-1 -translate-x-1/2 rounded-md {className}"
+  >
+    {text}
+  </div>
+{/if}
